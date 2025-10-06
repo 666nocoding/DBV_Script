6dd77ff5707abc93952e8e97a3a7cb79eac4296c
Winter <Winter@Winter.Winter>
2025-10-06 17:34:07 +0000

    这两文件意义不大。

--- dbv/version_linux.go
+++
@@ -1,5 +0,0 @@
-//go:build linux
-
-package dbv
-
-var version string = "dbv-linux-x86-64-v0.3.0"
--- dbv/version_windows.go
+++
@@ -1,5 +0,0 @@
-//go:build windows
-
-package dbv
-
-var version string = "dbv-windows-x86-64-v0.3.0"
